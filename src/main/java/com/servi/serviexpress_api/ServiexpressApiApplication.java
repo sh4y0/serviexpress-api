3bc3bc8694948a30973a0d970a315136ff32e7cd
Sh4y0 <Sh4y0>
2025-08-05 10:10:45 -0500

    chore(project): Set up initial folder structure using Clean Architecture and Vertical Slice

---
+++ src/main/java/com/servi/serviexpress_api/ServiexpressApiApplication.java
@@ -0,0 +1,13 @@
+package com.servi.serviexpress_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiexpressApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiexpressApiApplication.class, args);
+	}
+
+}
